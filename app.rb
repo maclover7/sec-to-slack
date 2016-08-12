4d5b0e0a0552f1c4c63ec647c5d1eafcf3ae1870
Jon Moss <me@jonathanmoss.me>
2016-08-12 12:17:56 -0400

    Add Slack notifications

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'sinatra/base'
 
+require 'slack-ruby-client'
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
 module SecToSlack
-  SEC_LISTS = [
-    'postmaster@jonathanmoss.me',
+  SEC_LISTS = ([
     'rubyonrails-security@googlegroups.com'
-  ].freeze
+  ] + ENV['SEC_LISTS']).freeze
 
   class Application < Sinatra::Base
     post '/' do
@@ -20,11 +24,51 @@ module SecToSlack
 
     def run
       lists = (SEC_LISTS & @email['To'].split(', '))
-      return unless lists
+      return unless lists.any?
 
-      #@notifier_params = {}
-      #@notifier_params['lists'] = lists
-      #@notifier_params['subject'] = @email['subject']
+      @notifier_params = {}
+      @notifier_params['lists'] = lists
+      @notifier_params['subject'] = @email['subject']
+
+      Notifier.new(@notifier_params).run
+    end
+  end
+
+  class Notifier
+    def initialize(params)
+      @params = params
+    end
+
+    def run
+      client = Slack::Web::Client.new
+      attachments = []
+
+      @params['lists'].each do |list|
+        title_link = ''
+
+        # Set title_link to the URL for the security google group
+        list_name = list.split('@googlegroups.com')
+        if list_name != [list]
+          title_link = "https://groups.google.com/forum/#!forum/#{list_name}"
+        end
+
+        a = {
+          fallback: "New email sent to #{list} - #{@params['subject']}",
+          title: "New email sent to '#{list}'",
+          title_link: title_link,
+          text: @params['subject'],
+          color: "#cb5234"
+        }
+
+        attachments << a
+      end
+
+      client.chat_postMessage({
+        channel: ENV['SLACK_ANN_CHANNEL'],
+        text: ':sirens: New CVEs have been released! Please start upgrading dependencies.',
+        as_user: true,
+        attachments: attachments
+      })
     end
   end
 end
